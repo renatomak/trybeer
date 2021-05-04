56eab76ef0be7328895901c99d396e8fca2cc974
renatomak <renato.mark.silva@gmail.com>
2021-05-04 16:50:07 -0300

    Funçoes de controle dos inputs

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/login" component={ Login } />
+    </Switch>
   );
 }
 
--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,49 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from './loginStyled';
 
-export default () => (
-  <Container>
-    <form>
-      <span>Email</span>
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="basic-addon1">@</span>
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeEmail = ({ target: { value } }) => {
+    setEmail(value);
+  };
+
+  const handleChangePassword = ({ target: { value } }) => {
+    setPassword(value);
+  };
+
+  const validatesEmail = () => {
+    const emailRegex = /[\w.-]+@[\w-]+\.[\w-.]+/gi;
+    return emailRegex.test(email);
+  };
+
+  return (
+    <Container>
+      <form>
+        <span>Email</span>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">@</span>
+            <input
+              type="email"
+              name="email"
+              value={ email }
+              placeholder="user@trybe.com"
+              data-testid="email-input"
+              onChange={ (e) => handleChangeEmail(e) }
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <span>Senha</span>
           <input
-            type="email"
-            name="email"
-            value="email"
+            type="password"
+            name="password"
+            value={ password }
+            placeholder="password"
             className="form-control"
-            aria-describedby="basic-addon1"
-            placeholder="user@trybe.com"
-            data-testid="email-input"
+            data-testid="password-input"
+            onChange={ (e) => handleChangePassword(e) }
           />
         </div>
-      </div>
-      <div className="form-group">
-        <span>Senha</span>
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          className="form-control"
-          data-testid="password-input"
-        />
-      </div>
-      <button
-        type="button"
-        data-testid="signin-btn"
-        className="btn btn-primary btn-lg btn-block"
-        value="Entrar"
-      >
-        Entrar
-      </button>
-      <button
-        type="button"
-        className="btn btn-outline-secondary btn-lg btn-block"
-        data-testid="no-account-btn"
-      >
-        Ainda nao tenho conta
-      </button>
-    </form>
-  </Container>
-);
+        <button
+          type="button"
+          data-testid="signin-btn"
+          className="btn btn-primary btn-lg btn-block"
+          value="Entrar"
+          disabled={ !validatesEmail }
+        >
+          Entrar
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-secondary btn-lg btn-block"
+          data-testid="no-account-btn"
+        >
+          Ainda nao tenho conta
+        </button>
+      </form>
+    </Container>
+  );
+};
