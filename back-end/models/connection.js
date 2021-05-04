9d3c99a282f207288a88a30316172013392f6cd8
Fernando Antunes <fernando.antunes1@gmail.com>
2021-05-04 16:00:21 -0300

    correção lint

--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -4,7 +4,7 @@ const config = {
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   host: process.env.HOSTNAME,
-  database: 'Trybeer'
+  database: 'Trybeer',
 };
 
 module.exports = config;
