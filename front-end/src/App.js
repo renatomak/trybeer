f14907935aa3f83bf8cd352ab48b60a42d94b2ee
renatomak <renato.mark.silva@gmail.com>
2021-05-04 18:08:11 -0300

    Funçoes de controle dos inputs - 2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Login from './pages/Login';
+import Products from './pages/Products';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/login" component={ Login } />
+      <Route path="/admin" component={ Home } />
+      <Route path="/products" component={ Products } />
+      <Route path="/register" component={ Register } />
     </Switch>
   );
 }
--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,21 +1,46 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Container } from './loginStyled';
 
-export default () => {
+function Login(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [buttonDisabled, setbuttonDisabled] = useState(true);
+
+  const validatesEmail = () => {
+    const emailRegex = /[\w.-]+@[\w-]+\.[\w-.]+/gi;
+    const validEmail = emailRegex.test(email);
+    const sizePassword = 5;
+    const validPassword = password.length >= sizePassword;
+
+    if (validEmail && validPassword) {
+      return setbuttonDisabled(false);
+    }
+
+    setbuttonDisabled(true);
+  };
 
   const handleChangeEmail = ({ target: { value } }) => {
     setEmail(value);
+    validatesEmail();
   };
 
   const handleChangePassword = ({ target: { value } }) => {
     setPassword(value);
+    validatesEmail();
   };
 
-  const validatesEmail = () => {
-    const emailRegex = /[\w.-]+@[\w-]+\.[\w-.]+/gi;
-    return emailRegex.test(email);
+  const logged = () => {
+    const { history } = props;
+    const user = { email, role: 'administrator', token: 'lafaljfhqoieun' };
+    localStorage.setItem('user', JSON.stringify(user));
+    if (user.role === 'administrator') history.push('/admin');
+    if (user.role === 'client') history.push('/products');
+  };
+
+  const register = () => {
+    const { history } = props;
+    history.push('/register');
   };
 
   return (
@@ -52,7 +77,8 @@ export default () => {
           data-testid="signin-btn"
           className="btn btn-primary btn-lg btn-block"
           value="Entrar"
-          disabled={ !validatesEmail }
+          disabled={ buttonDisabled }
+          onClick={ logged }
         >
           Entrar
         </button>
@@ -60,10 +86,19 @@ export default () => {
           type="button"
           className="btn btn-outline-secondary btn-lg btn-block"
           data-testid="no-account-btn"
+          onClick={ register }
         >
           Ainda nao tenho conta
         </button>
       </form>
     </Container>
   );
+}
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
+
+export default Login;
