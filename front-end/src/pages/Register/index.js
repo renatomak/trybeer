9a9be8bfd3689ce8f0ca139d6c5afffa8e0aeb16
samantabelow <samanta.below@gmail.com>
2021-05-05 16:02:56 -0300

    Faltando handleClick e validacao do nome

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -1,7 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default () => (
-  <div>
-    Cadastro de novo usuario
-  </div>
-);
+function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [checkbox, setCheckbox] = useState(false);
+  const [buttonDisabled, setbuttonDisabled] = useState(true);
+
+  const validateData = () => {
+    const validName = name.length >= 12;
+    const emailRegex = /[\w.-]+@[\w-]+\.[\w-.]+/gi;
+    const validEmail = emailRegex.test(email);
+    const sizePassword = 5;
+    const validPassword = password.length >= sizePassword;
+
+    if (validName && validEmail && validPassword) {
+      return setbuttonDisabled(false);
+    }
+    setbuttonDisabled(true);
+  };
+
+  const handleChangeName = ({ target: { value } }) => {
+    setName(value);
+    validateData();
+  };
+
+  const handleChangeEmail = ({ target: { value } }) => {
+    setEmail(value);
+    validateData();
+  };
+
+  const handleChangePassword = ({ target: { value } }) => {
+    setPassword(value);
+    validateData();
+  };
+
+  const handleChangeCheckbox = () => {
+    setCheckbox(!checkbox);
+  };
+
+  const handleClick = () => {
+
+  }
+
+  return (
+    <form>
+      <label htmlFor="signup-name">nome</label>
+      <input
+        data-testid="signup-name"
+        type="text"
+        name="name"
+        value={ name }
+        placeholder="nome"
+        onChange={ (e) => handleChangeName(e) }
+      ></input>
+      <label htmlFor="signup-email">email</label>
+      <input
+        data-testid="signup-email"
+        type="email"
+        name="email"
+        value={ email }
+        placeholder="user@trybe.com"
+        onChange={ (e) => handleChangeEmail(e) }
+      ></input>
+      <label htmlFor="signup-password">senha</label>
+      <input
+        data-testid="signup-password"
+        type="password"
+        name="password"
+        value={ password }
+        placeholder="senha"
+        onChange={ (e) => handleChangePassword(e) }
+      ></input>
+      <label htmlFor="signup-seller">quero vender</label>
+      <input
+        data-testid="signup-seller"
+        type="checkbox"
+        name="checkbox"
+        value="quero vender"
+        onChange={ handleChangeCheckbox }
+      ></input>
+      <button
+        type="button"
+        data-testid="signup-btn"
+        disabled={ buttonDisabled }
+        onClick={ handleClick }
+      >Cadastrar</button>
+    </form>
+  )
+};
+
+export default Register;
