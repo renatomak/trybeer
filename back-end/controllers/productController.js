58580f9cd9930b1ad3892c97fba69ebae969d6e3
Fernando Antunes <fernando.antunes1@gmail.com>
2021-05-07 18:18:52 -0300

    Endpoint de checkout salvando dados nas tabelas sales e sales_products

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const { productsServices } = require('../services/productsServices');
+const { productsServices, checkoutServices } = require('../services/productsServices');
 
 const {
   OK_200,
   UNAUTHORIZED_401 } = require('../util');
+
+  const internalError = 'Internal Error';
 
   const products = async (req, res) => {
     try {
@@ -12,19 +14,30 @@ const {
       res.status(OK_200).json(allProducts);
     } catch (err) {
       console.error(err.message);
-      res.status(UNAUTHORIZED_401).send({ message: 'Internal Error' });    
+      res.status(UNAUTHORIZED_401).send({ message: internalError });    
     }
   };
 
   const images = async (req, res) => {
-    const { imageName } = req.params;
-    const image = fs.createReadStream(path.join(__dirname, `../images/${imageName}`));
-    res.status(OK_200).sendFile(image.path); 
+    try {
+      const { imageName } = req.params;
+      const image = fs.createReadStream(path.join(__dirname, `../images/${imageName}`));
+      res.status(OK_200).sendFile(image.path); 
+    } catch (err) {
+      console.error(err.message);
+      res.status(UNAUTHORIZED_401).send({ message: internalError });    
+    }
   };
 
   const checkout = async (req, res) => {
-    const pedido = req.body;
-    res.status(OK_200).json({ message: 'Sucesso no controller', pedido });
+    try {
+      const pedido = req.body;
+      const retorno = await checkoutServices(pedido);
+      res.status(OK_200).json(retorno);
+    } catch (err) {
+      console.error(err.message);
+      res.status(UNAUTHORIZED_401).send({ message: internalError });    
+    }
   };
 
   module.exports = {
