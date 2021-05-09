4899b152bd1ac7c6ae953b2aee67aa365ac4d183
renatomak <renato.mark.silva@gmail.com>
2021-05-09 09:29:48 -0300

    lint

--- front-end/src/pages/Client/Ordens/CardOrder.js
+++ front-end/src/pages/Client/Ordens/CardOrder.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-const CardOrder = () => {
+const CardOrder = () => (
+  <div data-testid="0-order-card-container">
+    <span data-testid="0-order-number">Pedido 1</span>
+    <span data-testid="0-order-date"> 08/09 </span>
 
-  return (
-    <div data-testid="0-order-card-container">
-      <span data-testid="0-order-number">Pedido 1</span>
-      <span data-testid="0-order-date"> 08/09 </span>
-
-      <span data-testid="0-order-total-value">R$ 2,20</span>
-    </div>
-  );
-};
+    <span data-testid="0-order-total-value">R$ 2,20</span>
+  </div>
+);
 
 export default CardOrder;
