884fd22aed30af8bcc27a4100b022007d18e1663
renatomak <renato.mark.silva@gmail.com>
2021-05-10 15:57:07 -0300

    inicio do RQ7

--- front-end/src/pages/Client/Ordens/CardOrder.js
+++ front-end/src/pages/Client/Ordens/CardOrder.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CardOrder = () => (
-  <div data-testid="0-order-card-container">
-    <span data-testid="0-order-number">Pedido 1</span>
-    <span data-testid="0-order-date"> 08/09 </span>
+const CardOrder = (props) => {
+  const { order: { id, totalPrice, saleDate }, index } = props;
 
-    <span data-testid="0-order-total-value">R$ 2,20</span>
-  </div>
-);
+  const details = () => {
+    const { history } = props;
+    history.push('/orders/1');
+  };
+  return (
+    <button
+      type="button"
+      data-testid={ `${index}-order-card-container` }
+      onClick={ details }
+    >
+      <span data-testid={ `${index}-order-number` }>{ `Pedido ${id}` }</span>
+      <span data-testid={ `${index}-order-date` }>{saleDate}</span>
+
+      <span data-testid={ `${index}-order-total-value` }>{`R$ ${totalPrice}`}</span>
+    </button>);
+};
+
+CardOrder.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+  order: PropTypes.arrayOf
+};
 
 export default CardOrder;
--- front-end/src/pages/Client/Ordens/index.js
+++ front-end/src/pages/Client/Ordens/index.js
@@ -1,32 +1,22 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import SideBar from '../../components/SideBar';
 import CardOrder from './CardOrder';
-import { TrybeerContext } from '../../../util';
+import data from './data';
 
-const Ordens = (props) => {
-  const { shopCart } = useContext(TrybeerContext);
-  const details = () => {
-    const { history } = props;
-    history.push('/orders/1');
-  };
-
+const Ordens = () => {
   return (
     <div>
       <SideBar title="Meus Pedidos" />
       Meus pedidos
-      {shopCart.map((order) => <CardOrder order={ order } key={ order.id } />) }
-      <button type="button" onClick={ details }>
-        Pedido 1
-      </button>
+      {data
+        .map((order, index) => (<CardOrder
+          order={ order }
+          key={ order.id }
+          index={ index }
+        />)) }
     </div>
   );
-};
-
-Ordens.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
 };
 
 export default Ordens;
