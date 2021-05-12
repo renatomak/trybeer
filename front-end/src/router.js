c564731bd03737b9e3b3176eb291e6f1b0ec8ba3
samantabelow <samanta.below@gmail.com>
2021-05-12 16:30:49 -0300

    Lint

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
-import Home from './pages/Home';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Register from './pages/Register';
